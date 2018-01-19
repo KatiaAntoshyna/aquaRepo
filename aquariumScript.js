57df48e6f5366b9a35dfc03106574d93589b969d
KatiaAntoshyna <akaterink@gmail.com>
2018-01-19 14:13:19 +0200

    okay, maybe toString will help

--- aquariumScript.js
+++ aquariumScript.js
@@ -14,10 +14,10 @@ var states = 10101; // later read this from http!!!
 
 document.getElementById("justP").innerHTML =
   "states = 1" +
-  switchA.activity +
-  switchL.activity +
-  switchF.activity +
-  switchO.activity;
+  switchA.activity.toString() +
+  switchL.activity.toString() +
+  switchF.activity.toString() +
+  switchO.activity.toString();
 
 function changeButton(name) {
   if (document.getElementById(name).style.backgroundColor == "rgb(52, 99, 5)") {
