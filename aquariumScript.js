ed10a74cfd530ad19a459a0dd118950148f2bc2e
KatiaAntoshyna <akaterink@gmail.com>
2018-01-23 11:54:25 +0200

    i try to change states

--- aquariumScript.js
+++ aquariumScript.js
@@ -1,47 +1,56 @@
 //require("serverScript.js");
 
 function Tumblr(name, num) {
-  var a = Math.floor((states % Math.pow(10, num + 1)) / Math.pow(10, num));
+  var a = (states % Math.pow(10, num + 1)) / Math.pow(10, num);
   this.activity = +a;
-  this.color = document.getElementById(name).style.backgroundColor;
+  if ((a = 0)) {
+    document.getElementById(name).style.backgroundColor = "red";
+    this.color = document.getElementById(name).style.backgroundColor;
+  } else if ((a = 1)) {
+    document.getElementById(name).style.backgroundColor = "rgb(52, 99, 5)";
+    this.color = document.getElementById(name).style.backgroundColor;
+  } else document.getElementById("justP").innerHTML = "error!";
 }
 
-var switchA = new Tumblr("AIR", 4);
-var switchL = new Tumblr("LIGHT", 3);
-var switchF = new Tumblr("FILTER", 2);
-var switchO = new Tumblr("OTHER", 1);
-var states = 10101; // later read this from http!!!
+var states = 10110; // later read this from http!!!
+var AIR = new Tumblr("AIR", 4);
+var LIGHT = new Tumblr("LIGHT", 3);
+var FILTER = new Tumblr("FILTER", 2);
+var OTHER = new Tumblr("OTHER", 1);
 
 document.getElementById("justP").innerHTML =
   "states = 1" +
-  switchA.activity +
-  switchL.activity +
-  switchF.activity +
-  switchO.activity;
+  AIR.activity +
+  LIGHT.activity +
+  FILTER.activity +
+  OTHER.activity;
 
 function changeButton(name) {
   if (document.getElementById(name).style.backgroundColor == "rgb(52, 99, 5)") {
     document.getElementById(name).style.backgroundColor = "red";
-  } else document.getElementById(name).style.backgroundColor = "rgb(52, 99, 5)";
+  } else {
+    document.getElementById(name).style.backgroundColor = "rgb(52, 99, 5)";
+  }
+  this.color = document.getElementById(name).style.backgroundColor;
 }
 
 function switchActivity(name) {
-  if (name.color == "rgb(52, 99, 5)") {
-    name.activity = 1;
-  } else name.activity = 2;
+  if (this.color == "rgb(52, 99, 5)") {
+    this.activity = 1;
+  } else this.activity = 0;
 }
 
 function okDealer() {
-  switchActivity(switchA);
-  switchActivity(switchL);
-  switchActivity(switchF);
-  switchActivity(switchO);
+  switchActivity(AIR);
+  switchActivity(LIGHT);
+  switchActivity(FILTER);
+  switchActivity(OTHER);
   // send a http (?) signal to arduino
   states =
     10000 +
-    1000 * switchA.activity +
-    100 * switchL.activity +
-    10 * switchF.activity +
-    switchO.activity;
+    1000 * AIR.activity +
+    100 * LIGHT.activity +
+    10 * FILTER.activity +
+    OTHER.activity;
   document.getElementById("justP").innerHTML = "states = " + states;
 }
